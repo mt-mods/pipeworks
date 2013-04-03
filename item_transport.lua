e271dc0818ee61e59427f29faf3990db88ba497d
bas080 <bas080@hotmail.com>
2013-04-03 12:16:33 +0200

    retouched the formspec of the filter and mese-filter

--- item_transport.lua
+++ item_transport.lua
@@ -18,9 +18,9 @@ minetest.register_node("pipeworks:filter", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-				"invsize[9,9;]"..
-				"list[current_name;main;0,2;8,2;]"..
-				"list[current_player;main;0,5;8,4;]")
+				"invsize[8,6.5;]"..
+				"list[current_name;main;0,0;8,2;]"..
+				"list[current_player;main;0,2.5;8,4;]")
 		meta:set_string("infotext", "Filter")
 		local inv = meta:get_inventory()
 		inv:set_size("main", 8*4)
@@ -113,9 +113,9 @@ minetest.register_node("pipeworks:mese_filter", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-				"invsize[9,9;]"..
-				"list[current_name;main;0,2;8,2;]"..
-				"list[current_player;main;0,5;8,4;]")
+				"invsize[8,6.5;]"..
+				"list[current_name;main;0,0;8,2;]"..
+				"list[current_player;main;0,2.5;8,4;]")
 		meta:set_string("infotext", "Mese filter")
 		local inv = meta:get_inventory()
 		inv:set_size("main", 8*4)
