e4a7af5858949ed786396abf0edd7f98c1e28941
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2013-12-10 21:18:29 -0500

    Better way to catch that incoming nil, return a real value on the way out

--- autoplace_tubes.lua
+++ autoplace_tubes.lua
@@ -28,6 +28,8 @@ end
 
 --a function for determining which side of the node we are on
 local function nodeside(node, tubedir)
+	if not tubedir or not node or not node.param2 then return "back" end
+
     --get a vector pointing back
     local backdir = minetest.facedir_to_dir(node.param2)
 
