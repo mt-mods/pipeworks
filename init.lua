15fe8e068f8e01d84215cf79db8380fdfe22a32d
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2017-04-11 19:16:21 -0400

    cache button "base" field since it's also standard

--- init.lua
+++ init.lua
@@ -41,8 +41,9 @@ pipeworks.mesecons_rules={{x=0,y=0,z=1},{x=0,y=0,z=-1},{x=1,y=0,z=0},{x=-1,y=0,z
 
 pipeworks.liquid_texture = "default_water.png"
 
-pipeworks.button_off = {text="", texture="pipeworks_button_off.png", addopts="false;false;pipeworks_button_interm.png"}
-pipeworks.button_on  = {text="", texture="pipeworks_button_on.png",  addopts="false;false;pipeworks_button_interm.png"}
+pipeworks.button_off   = {text="", texture="pipeworks_button_off.png", addopts="false;false;pipeworks_button_interm.png"}
+pipeworks.button_on    = {text="", texture="pipeworks_button_on.png",  addopts="false;false;pipeworks_button_interm.png"}
+pipeworks.button_base  = "image_button[0,4.3;1,0.6"
 pipeworks.button_label = "label[0.9,4.31;Allow splitting incoming stacks from tubes]"
 
 -- Helper functions
