d4b32d5fa332fe2d3aec8a433bcde3abb80406ff
thetaepsilon-gamedev <thetaepsilon-gamedev@noreply.users.github.com>
2017-10-19 21:41:04 +0100

    default_settings.lua: document pressure logic settings

--- default_settings.lua
+++ default_settings.lua
@@ -29,6 +29,25 @@ local settings = {
 	delete_item_on_clearobject = true,
 }
 
+-- documentation for toggles controlling pressure logic features
+-- do not edit this;
+-- instead, copy the uncommented lines into pipeworks_settings.txt in your world directory.
+--[[
+-- enable pressure logic mode instead of "classic" mode.
+-- WARNING: this changes a few things, most noticeably how pumps work.
+-- you'll want to make sure they're fed by an infinite spring.
+pipeworks.toggles.pressure_logic = true
+
+-- force-enable finite water handling mode.
+-- this changes the way that water node placement is handled;
+-- volume will always be preserved, and water is assumed to move itself downwards.
+-- nil (the default) means autodetect from installed finite liquid mods, true is force-on, false is force-off.
+-- note that you should NOT normally explicitly set this to either true or false,
+-- unless the mod you want this for is not covered by autodetect-finite-water.lua.
+-- please file an issue if you need to use this for a finite water mod exists not covered there.
+pipeworks.toggles.finite_water = nil
+]]
+
 for name, value in pairs(settings) do
 	local setting_type = type(value)
 	if setting_type == "boolean" then
