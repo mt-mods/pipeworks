21e6ba12c4e5ac87dbec1247afdff49338e691f7
zichen <34205903+IFRFSX@users.noreply.github.com>
2021-12-26 13:25:43 +0800

    Update autocrafter.lua
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

--- autocrafter.lua
+++ autocrafter.lua
@@ -277,8 +277,7 @@ minetest.register_node("pipeworks:autocrafter", {
 		update_meta(meta, false)
 	end,
 	on_receive_fields = function(pos, formname, fields, sender)
-		if (fields.quit and not fields.key_enter_field)
-				or not pipeworks.may_configure(pos, sender) then
+		if (fields.quit and not fields.key_enter_field) or not pipeworks.may_configure(pos, sender) then
 			return
 		end
 		local meta = minetest.get_meta(pos)
