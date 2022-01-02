794cae675e3e065077239e0e14391067a8c48ae6
SX <50966843+S-S-X@users.noreply.github.com>
2022-01-02 16:14:14 +0200

    Expose set_tube on public API (#6)
    * Expose set_tube on public API
    * Added update_meta to public tptube API

--- teleport_tube.lua
+++ teleport_tube.lua
@@ -287,5 +287,7 @@ pipeworks.tptube = {
 	hash = hash,
 	save_tube_db = save_tube_db,
 	get_db = function() return tp_tube_db or read_tube_db() end,
+	set_tube = set_tube,
+	update_meta = update_meta,
 	tp_tube_db_version = tp_tube_db_version
 }
