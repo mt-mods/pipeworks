6d2db5f9c4a187621328f185932e463a42d93999
unknown <24964441+wsor4035@users.noreply.github.com>
2022-03-05 22:04:59 -0500

    fix wierd tab spacing

--- crafts.lua
+++ crafts.lua
@@ -1,87 +1,87 @@
 -- Crafting recipes for pipes
 
 minetest.register_craft( {
-        output = "pipeworks:pipe_1_empty 12",
-        recipe = {
-                { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" },
-                { "", "", "" },
-                { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" }
-        },
+    output = "pipeworks:pipe_1_empty 12",
+    recipe = {
+            { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" },
+            { "", "", "" },
+            { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" }
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:straight_pipe_empty 3",
-        recipe = {
-                { "pipeworks:pipe_1_empty", "pipeworks:pipe_1_empty", "pipeworks:pipe_1_empty" },
-        },
+    output = "pipeworks:straight_pipe_empty 3",
+    recipe = {
+            { "pipeworks:pipe_1_empty", "pipeworks:pipe_1_empty", "pipeworks:pipe_1_empty" },
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:spigot 3",
-        recipe = {
-                { "pipeworks:pipe_1_empty", "" },
-                { "", "pipeworks:pipe_1_empty" },
-        },
+    output = "pipeworks:spigot 3",
+    recipe = {
+            { "pipeworks:pipe_1_empty", "" },
+            { "", "pipeworks:pipe_1_empty" },
+    },
 })
 
 minetest.register_craft( {
-	output = "pipeworks:entry_panel_empty 2",
-	recipe = {
-		{ "", "default:steel_ingot", "" },
-		{ "", "pipeworks:pipe_1_empty", "" },
-		{ "", "default:steel_ingot", "" },
-	},
+output = "pipeworks:entry_panel_empty 2",
+recipe = {
+    { "", "default:steel_ingot", "" },
+    { "", "pipeworks:pipe_1_empty", "" },
+    { "", "default:steel_ingot", "" },
+},
 })
 
 -- Various ancillary pipe devices
 
 minetest.register_craft( {
-        output = "pipeworks:pump_off 2",
-        recipe = {
-                { "default:stone", "default:steel_ingot", "default:stone" },
-                { "default:copper_ingot", "default:mese_crystal_fragment", "default:copper_ingot" },
-                { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" }
-        },
+    output = "pipeworks:pump_off 2",
+    recipe = {
+            { "default:stone", "default:steel_ingot", "default:stone" },
+            { "default:copper_ingot", "default:mese_crystal_fragment", "default:copper_ingot" },
+            { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" }
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:valve_off_empty 2",
-        recipe = {
-                { "", "group:stick", "" },
-                { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" },
-                { "", "default:steel_ingot", "" }
-        },
+    output = "pipeworks:valve_off_empty 2",
+    recipe = {
+            { "", "group:stick", "" },
+            { "default:steel_ingot", "default:steel_ingot", "default:steel_ingot" },
+            { "", "default:steel_ingot", "" }
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:storage_tank_0 2",
-        recipe = {
-                { "", "default:steel_ingot", "default:steel_ingot" },
-                { "default:steel_ingot", "default:glass", "default:steel_ingot" },
-                { "default:steel_ingot", "default:steel_ingot", "" }
-        },
+    output = "pipeworks:storage_tank_0 2",
+    recipe = {
+            { "", "default:steel_ingot", "default:steel_ingot" },
+            { "default:steel_ingot", "default:glass", "default:steel_ingot" },
+            { "default:steel_ingot", "default:steel_ingot", "" }
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:grating 2",
-        recipe = {
-                { "default:steel_ingot", "", "default:steel_ingot" },
-                { "", "pipeworks:pipe_1_empty", "" },
-                { "default:steel_ingot", "", "default:steel_ingot" }
-        },
+    output = "pipeworks:grating 2",
+    recipe = {
+            { "default:steel_ingot", "", "default:steel_ingot" },
+            { "", "pipeworks:pipe_1_empty", "" },
+            { "default:steel_ingot", "", "default:steel_ingot" }
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:flow_sensor_empty 2",
-        recipe = {
-                { "pipeworks:pipe_1_empty", "mesecons:mesecon", "pipeworks:pipe_1_empty" },
-        },
+    output = "pipeworks:flow_sensor_empty 2",
+    recipe = {
+            { "pipeworks:pipe_1_empty", "mesecons:mesecon", "pipeworks:pipe_1_empty" },
+    },
 })
 
 minetest.register_craft( {
-        output = "pipeworks:fountainhead 2",
-        recipe = {
-                { "pipeworks:pipe_1_empty" },
-				{ "pipeworks:pipe_1_empty" }
-        },
+    output = "pipeworks:fountainhead 2",
+    recipe = {
+            { "pipeworks:pipe_1_empty" },
+            { "pipeworks:pipe_1_empty" }
+    },
 })
