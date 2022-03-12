0518f490037eaa95c8ea0acedb2756f80f7621eb
unknown <24964441+wsor4035@users.noreply.github.com>
2022-03-12 16:42:45 -0500

    strip materials list down to only whats needed

--- crafts.lua
+++ crafts.lua
@@ -1,114 +1,61 @@
 local materials = {
-	dirt = "default:dirt",
-	sand = "default:sand",
+	stone = "default:stone",
 	desert_stone = "default:desert_stone",
 	desert_sand = "default:desert_sand",
 	chest = "default:chest",
-	gravel = "default:gravel",
 	copper_ingot = "default:copper_ingot",
 	steel_ingot = "default:steel_ingot",
 	gold_ingot = "default:gold_ingot",
-	tin_ingot = "default:tin_ingot",
     mese = "default:mese",
     mese_crystal = "default:mese_crystal",
 	mese_crystal_fragment = "default:mese_crystal_fragment",
-	torch = "default:torch",
-	diamond = "default:diamond",
-	clay_lump = "default:clay_lump",
-	water_bucket = "bucket:bucket_water",
-	empty_bucket = "bucket:bucket_empty",
-	dye_dark_grey = "dye:dark_grey",
-	silicon = "mesecons_materials:silicon",
-    stone = "default:stone",
     glass = "default:glass",
 }
 
 if minetest.get_modpath("mcl_core") then
 	materials = {
-		dirt = "mcl_core:dirt",
-		sand = "mcl_core:sand",
-		gravel = "mcl_core:gravel",
+		stone = "fl_stone:stone",
+		desert_stone = "fl_stone:desert_stone",
+		desert_sand = "fl_stone:desert_sand",
+		chest = "default:chest",
 		steel_ingot = "mcl_core:iron_ingot",
 		gold_ingot = "mcl_core:gold_ingot",
         mese = "default:mese",
         mese_crystal = "default:mese_crystal",
 		mese_crystal_fragment = "mesecons:redstone",
-		torch = "mcl_torches:torch",
-		diamond = "mcl_core:diamond",
-		clay_lump = "mcl_core:clay_lump",
-		water_bucket = "mcl_buckets:bucket_water",
-		empty_bucket = "mcl_buckets:bucket_empty",
-		dye_dark_grey = "mcl_dye:dark_grey",
 		-- Use iron where no equivalent
 		copper_ingot = "mcl_core:iron_ingot",
-		tin_ingot = "mcl_core:iron_ingot",
-		silver_ingot = "mcl_core:iron_ingot",
-		silicon = "mesecons_materials:silicon",
-        stone = "default:stone",
         glass = "default:glass",
 	}
 elseif minetest.get_modpath("fl_ores") and minetest.get_modpath("fl_stone") then
 	materials = {
-		dirt = "fl_topsoil:dirt",
-		sand = "fl_stone:sand",
+		stone = "fl_stone:stone",
 		desert_stone = "fl_stone:desert_stone",
 		desert_sand = "fl_stone:desert_sand",
 		chest = "fl_storage:wood_chest",
-		gravel = "fl_topsoil:gravel",
 		steel_ingot = "fl_ores:iron_ingot",
 		gold_ingot = "fl_ores:gold_ingot",
         mese = "fl_ores:iron_ingot",
         mese_crystal = "fl_ores:iron_ingot",
 		mese_crystal_fragment = "fl_ores:iron_ingot",
-		torch = "fl_light_sources:torch",
-		diamond = "fl_ores:diamond",
-		clay_lump = "fl_bricks:clay_lump",
-		water_bucket = "fl_bucket:bucket_water",
-		empty_bucket = "fl_bucket:bucket",
-		dye_dark_grey = "fl_dyes:dark_grey_dye",
 		copper_ingot = "fl_ores:copper_ingot",
-		tin_ingot = "fl_ores:tin_ingot",
-		silver_ingot = "fl_ores:iron_ingot",
-		silicon = "mesecons_materials:silicon",
-        stone = "fl_stone:stone",
         glass = "fl_glass:framed_glass",
 	}
 elseif minetest.get_modpath("hades_core") then
 	materials = {
-		dirt = "hades_core:dirt",
-		sand = "hades_core:fertile_sand",
-		gravel = "hades_core:gravel",
+		stone = "hades_core:stone",
+		desert_stone = "hades_core:desert_stone",
+		desert_sand = "hades_core:desert_sand",
+		chest = "hades_core:chest";
 		steel_ingot = "hades_core:steel_ingot",
 		gold_ingot = "hades_core:gold_ingot",
-        mese = "default:mese",
-        mese_crystal = "default:mese_crystal",
+        mese = "hades_core:mese",
+        mese_crystal = "hades_core:mese_crystal",
 		mese_crystal_fragment = "hades_core:mese_crystal_fragment",
-		torch = "hades_torches:torch",
-		diamond = "hades_core:diamond",
-		clay_lump = "hades_core:clay_lump",
-		dye_dark_grey = "dye:dark_grey",
 		copper_ingot = "hades_core:copper_ingot",
 		tin_ingot = "hades_core:tin_ingot",
-		--[[
-			Since hades doesnt have buckets or water for the user,
-			using dirt from near water to pull the water out
-		]]
-		water_bucket = "hades_core:dirt",
-		empty_bucket = "hades_core:fertile_sand",
-		-- Set this to steel unless hadesextraores is present
-		silver_ingot = "hades_core:steel_ingot",
-		silicon = "hades_materials:silicon",
-        stone = "default:stone",
         glass = "default:glass",
 	}
-
-	if minetest.get_modpath("hades_bucket") then
-		materials["water_bucket"] = "hades_bucket:bucket_water"
-		materials["empty_bucket"] = "hades_bucket:bucket_empty"
-	end
-	if minetest.get_modpath("hades_extraores") then
-		materials["silver_ingot"] = "hades_extraores:silver_ingot"
-	end
 end
 
 -- Crafting recipes for pipes
