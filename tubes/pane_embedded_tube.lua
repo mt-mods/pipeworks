ad7bf2521c242a584931eeb66b07fc0d6c512d60
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-01-12 14:02:57 +0100

    code and registrations cleanup

--- tubes/pane_embedded_tube.lua
+++ tubes/pane_embedded_tube.lua
@@ -1,39 +1,6 @@
 local S = minetest.get_translator("pipeworks")
 
 local straight = function(pos, node, velocity, stack) return {velocity} end
-local steel_tex = "[combine:16x16^[noalpha^[colorize:#D3D3D3"
-if minetest.get_modpath("default") then steel_tex = "default_steel_block.png" end
-
-minetest.register_node("pipeworks:steel_block_embedded_tube", {
-	description = S("Airtight steelblock embedded tube"),
-	tiles = {
-		steel_tex, steel_tex,
-		steel_tex, steel_tex,
-		steel_tex .. "^pipeworks_tube_connection_metallic.png",
-		steel_tex .. "^pipeworks_tube_connection_metallic.png",
-		},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {cracky=1, oddly_breakable_by_hand = 1, tubedevice = 1, dig_glass = 2, pickaxey=1, handy=1},
-	_mcl_hardness=0.8,
-	legacy_facedir_simple = true,
-	_sound_def = {
-        key = "node_sound_stone_defaults",
-    },
-	tube = {
-		connect_sides = {front = 1, back = 1,},
-		priority = 50,
-		can_go = straight,
-		can_insert = function(pos, node, stack, direction)
-			local dir = minetest.facedir_to_dir(node.param2)
-			return vector.equals(dir, direction) or vector.equals(vector.multiply(dir, -1), direction)
-		end,
-	},
-	after_place_node = pipeworks.after_place,
-	after_dig_node = pipeworks.after_dig,
-	on_rotate = pipeworks.on_rotate,
-})
-pipeworks.ui_cat_tube_list[#pipeworks.ui_cat_tube_list+1] = "pipeworks:steel_block_embedded_tube"
 
 local pane_box = {
 	type = "fixed",
