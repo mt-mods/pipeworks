mt-mods/pipeworks

commit dcc62eb23143ea60411a0d0ea5ee8fb68e997696

Fixes Issue #64 (#142)
* Fixes Issue #64

Uses a simple boolean setting in default_settings.lua to decide which digiline rules to use in common.lua

* Reviewer Suggestion

Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

* Changed Code Comment per Reviewer Suggestion

* Removed trailing whitespace from 2 comment lines.

* Modified settings comment per reviewer suggestion

* Changed variable name per reviewer suggestion.

Variable changed from use_default_digilines_rules to enable_vertical_digilines_connectivity.

---------

Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

default_settings.lua
@@ -31,6 +31,7 @@ local settings = {
 	delete_item_on_clearobject = true,
 	use_real_entities = true,
 	entity_update_interval = 0,
+	enable_vertical_digilines_connectivity = true,
 }
 
 pipeworks.toggles = {}

common.lua
@@ -15,7 +15,17 @@ pipeworks.rules_all = {{x=0, y=0, z=1},{x=0, y=0, z=-1},{x=1, y=0, z=0},{x=-1, y
 		{x=0, y=1, z=0}, {x=0, y=-1, z=0}}
 
 pipeworks.mesecons_rules={{x=0,y=0,z=1},{x=0,y=0,z=-1},{x=1,y=0,z=0},{x=-1,y=0,z=0},{x=0,y=1,z=0},{x=0,y=-1,z=0}}
-pipeworks.digilines_rules={{x=0,y=0,z=1},{x=0,y=0,z=-1},{x=1,y=0,z=0},{x=-1,y=0,z=0},{x=0,y=1,z=0},{x=0,y=-1,z=0}}
+
+local digilines_enabled = minetest.get_modpath("digilines") ~= nil
+if digilines_enabled and pipeworks.enable_vertical_digilines_connectivity then
+	pipeworks.digilines_rules=digiline.rules.default
+else
+	-- These rules break vertical connectivity to deployers, node breakers, dispensers, and digiline filter injectors
+	-- via digiline conducting tubes. Changing them may break some builds on some servers, so the setting was added
+	-- for server admins to be able to revert to the old "broken" behavior as some builds may use it as a "feature".
+	-- See https://github.com/mt-mods/pipeworks/issues/64
+	pipeworks.digilines_rules={{x=0,y=0,z=1},{x=0,y=0,z=-1},{x=1,y=0,z=0},{x=-1,y=0,z=0},{x=0,y=1,z=0},{x=0,y=-1,z=0}}
+end
 
 pipeworks.liquid_texture = minetest.registered_nodes[pipeworks.liquids.water.flowing].tiles[1]
 if type(pipeworks.liquid_texture) == "table" then pipeworks.liquid_texture = pipeworks.liquid_texture.name end

commit 858154cb78ae773768956cf132886bc6a3e89027

Fix #145 caused by #142 Make new feature opt-in  (#146)
* Update default_settings.lua and settingtypes.txt
- Make new vertical behaviour opt-in
- crop comment

default_settings.lua
@@ -31,7 +31,7 @@ local settings = {
 	delete_item_on_clearobject = true,
 	use_real_entities = true,
 	entity_update_interval = 0,
-	enable_vertical_digilines_connectivity = true,
+	enable_vertical_digilines_connectivity = false,
 }
 
 pipeworks.toggles = {}