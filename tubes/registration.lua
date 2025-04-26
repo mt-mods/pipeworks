54c9f2de989519b8c895316fe0694caae55399b2
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-04-26 12:16:18 -0500

    Update registration.lua

--- tubes/registration.lua
+++ tubes/registration.lua
@@ -61,13 +61,13 @@ local register_one_tube = function(name, tname, dropname, desc, plain, noctrs, e
 		outimgs[vti[v]] = ends[v]
 	end
 
-	local tgroups = {snappy = 3, tube = 1, tubedevice = 1, not_in_creative_inventory = 1, dig_generic = 4, axey=1, handy=1, pickaxey=1}
+	local tgroups = {snappy = 3, tube = 1, tubedevice = 1, tube = 1, not_in_creative_inventory = 1, dig_generic = 4, axey=1, handy=1, pickaxey=1}
 	local tubedesc = string.format("%s %s", desc, dump(connects))
 	local iimg = type(plain[1]) == "table" and plain[1].name or plain[1]
 	local wscale = {x = 1, y = 1, z = 1}
 
 	if #connects == 0 then
-		tgroups = {snappy = 3, tube = 1, tubedevice = 1, dig_generic = 4, axey=1, handy=1, pickaxey=1}
+		tgroups = {snappy = 3, tube = 1, tubedevice = 1, tube = 1, dig_generic = 4, axey=1, handy=1, pickaxey=1}
 		tubedesc = desc
 		iimg=inv
 		outimgs = {
