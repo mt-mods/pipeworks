b6ce18f2aa54896e9580167184462cad9bee8eec
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-02-15 07:27:40 -0500

    relocate "matching" filter

--- filter-injector.lua
+++ filter-injector.lua
@@ -280,11 +280,20 @@ local function punch_filter(data, filtpos, filtnode, msg)
 			local matches
 			if filterfor == "" then
 				matches = stack:get_name() ~= ""
+				if (not fname) and matching_mode == 1 then
+					filterfor = {name = stack:get_name()}
+				end
 			else
 				local fname = filterfor.name
 				local fgroup = filterfor.group
 				local fwear = filterfor.wear
 				local fmetadata = filterfor.metadata
+				
+				if (not fname) and matching_mode == 1 then
+					fname = stack:get_name()
+					filterfor.name = fname
+				end
+				
 				matches = (not fname                                             -- If there's a name filter,
 				           or stack:get_name() == fname)                         --  it must match.
 
@@ -342,7 +351,6 @@ local function punch_filter(data, filtpos, filtnode, msg)
 			end
 			-- stupid lack of continue statements grumble
 			if doRemove > 0 then
-				if matching_mode == 1 then filterfor.name = stack:get_name() end
 				if slotseq_mode == 2 then
 					local nextpos = spos + 1
 					if nextpos > frominv:get_size(frominvname) then
